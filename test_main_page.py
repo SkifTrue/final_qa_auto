860f74b70b7f7efa566cc1a6e4a994bbf42d8490
SkifTrue <vasili.trutnev@gmail.com>
2024-07-25 22:12:39 +0500

    LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,17 @@
-from .pages.main_page import MainPage
-
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
