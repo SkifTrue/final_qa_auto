78fac2c700aa8cf77fb4843285b98f43ec5bd039
SkifTrue <vasili.trutnev@gmail.com>
2024-07-26 01:07:44 +0500

    finish

--- test_product_page.py
+++ test_product_page.py
@@ -1,23 +1,25 @@
 import pytest
 from pages.product_page import ProductPage
+from selenium.webdriver.common.by import By
 
-@pytest.mark.parametrize('link', [
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
-])
-def test_guest_can_add_product_to_basket(browser, link):
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_success_message()
-    page.should_be_correct_product_name()
-    page.should_be_correct_basket_total()
+    page.is_not_element_present(By.CSS_SELECTOR, ".alert-success")  # Передаем 'how' и 'what'
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.is_not_element_present(By.CSS_SELECTOR, ".alert-success")  # Передаем 'how' и 'what'
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.is_disappeared(By.CSS_SELECTOR, ".alert-success")  # Передаем 'how' и 'what'
